a8b9f68d909efa1a2f6c50416438d46b6eb208b6
Jaime Elepano <jaime@Jaimes-MacBook-Air-939.local>
2025-02-04 20:45:48 -0800

    restructured code

--- README.md
+++ README.md
@@ -15,10 +15,12 @@ The purpose of the script is to copy each entry into a new sheet and duplicate e
 For example: If Prof. Smith sends a request for Math 1 and Phys 50, which have 5 and 2 sections respectively, we would be inserting
 7 new rows into the duplicates table. Ideally we'd also be filtering out data based on that section. So for the 5 Math 1 sections, the first entry would have the section specific info for just that section.
 
-**Functions**
-- AppendRow(): Append the specified row to the section duplic ates list, duplicating by sections as specified above (You must trigger this manually and specify the row in the Row Variable)
-- AppendNewestRow(): Append the newest row to the end of the section duplicates list, duplicating by sections as specified above. (Manually triggered in Google Apps Script Editor/Terminal)
-- AppendAllRows(): Run the duplication process as specified above on the entire table (Can be manually triggered, but is also linked to a trigger to automatically run every time the sheet is opened)
+Unless a large scale change is requested, your only work will be to test the script to ensure it works as intended. The current version hardcodes certain column values. As the source sheet may change in structure, your main job in terms of maintenance is to ensure all the hardcoded values are accurate.
+
+**Main Functions**
+- AppendSingleRow(): Append the specified row to the section duplicates list, duplicating by sections as specified above (You must trigger this manually and specify the row in the Row Variable)
+- AppendNewRows(): Append all rows that don't have the "Duplicated" column (usually the last column) checked. (Can be manually triggered, but is also linked to a trigger to automatically run every time the sheet is opened)
+- AppendAllRows(): Run the duplication process as specified above on the entire table indiscriminately (Should be manually run in the event the duplicates table needs many or all its entries updated)
 
 **Triggers**
 As of now, applications are collected through Qualtrics and logged into the Google Sheet this script is attached to. Due to the varying levels of access required due to these scripts being maintained by Peer Educators, at the time of writing, a solution to trigger functions everytime a new entry comes in has not yet been develped. 
@@ -26,3 +28,27 @@ As of now, applications are collected through Qualtrics and logged into the Goog
 There is talk about changing the system, but for the time being, the code has a trigger set to run AppendAllRows() whenever someone opens the sheet. That way, every time the sheet is opened, any new entries in the main sheet will be automatically ended.
 
 *NOTE*: For the time being, the code is run on my (Jaime Elepano) Google Account. Should ownership of the Google Apps Script Project be transferred to your account, every time the code executes (ie. every time someone opens the sheet), your Google Account will show up in the edit history, as the automated actions are run from your gmail account. Exercise caution as you are handling sensitive data.
+
+**Underlying Logic**
+
+*Notes*
+Columns are indexed by number, not letter. I've written a helper function ```colNum(<column letter>)``` to simplify the process of specifying columns
+
+*Duplicating a Single Row*
+1. Checking if the specific row has already been duplicated. There should be a checkbox in the very last cell of each row in the sourcesheet to represent this. If it isn't already, please label this column for ease of comprehension. The variable ```dCheckColumn``` stores this column's number in the code. Make sure to hardcode this as needed with each iteration of the sheet.
+2. Next we want to know how many section requests there are. The utility function ```setRequests(SpecificRow)``` does this. Just specify the row as its parameter. Once run, the request count is stored in a variable
+3. Generate the ranges of included values As mentioned before, for each section's entry, there is no need to specify all the other sections' information. The helper function ```getRanges(i)``` does all the work. It takes the current i (indicating what course you're making duplicates of) as a parameter.
+4. Next Build the row. The ```buildRow(range)``` function does all the work. You'll need to specify the included range as the parameter (calculated in Step 3). It iterates through each column index and checks if its in the range specified in its parameter, then appends that value to an array. The returned array is stored in the variable ```builtRow```.
+5. Finally, use the built array to append a the new row to the duplicates table. The function ```appendTable(builtRow, sectionCount)``` does all thework. You'll need to push the built arraw and the number of sections as parameters respectively. If you're wondering about the getRange() call in the function, here's the call structure ```getRange(startRow, startColumn, optionalNumberOfRows, optionalNumberOfColumns)```
+
+*Duplicating New Rows/All Rows*
+The logic builds on that of duplicating a single row.
+
+1. Since you'll be duplicating multiple rows, you'll want to use the function ```filledCount()``` to get a count of filled rows.
+2. Essentially all you have to do now is steps 2 - 5 of duplicating a single row, but for each new entry. Simply put the same code in a loop. If you're only duplicating new row, within the loop, create a check for if that row has already been duplicated. (We could simply look for the first value without the duplication check mark since theoretically all those after would be new as well, but we're iterating over the whole list to be safe.). 
+
+Note: The code originally had the code rewritten for each function due to the developer (Jaime) being lazy, plus the ```appendRow()``` function had to be runnable without parameters so that less tech savvy users could run it standalone, meaning it couldn't be used as a driver function. Now the code has been restructured and ```appendRow()``` is a driver function while ```appendSingleRow()``` is the one executed manually.
+
+3. Rememeber after each loop to mark the row as duplicated. The code should already have a line to do this.
+
+For any questions contact Jaime Elepano: (jaimeelepano2357@gmail.com)
--- main.js
+++ main.js
@@ -1,283 +1,142 @@
-// Instructions for Usage:
+// Simple Instructions for Usage:
 
-// Select the function you'd like to use from the dropdown menu above (next to Debug):
-//     - appendRow: Appends the selected row to the last row of the new sheet 
-//       based on section and request count. (
-//.      Notes:
-//          *Change row Variable to correct row number - specified by dashes)
-//          *Make sure to click Save (Button to the left of "Run") before clicking Run
-//     - appendAllRows: Takes all rows in the sheet and appends them to the new sheet 
-//       based on section and request count.
-// Then Click run
-//     - A text box will pop up, this will inform you when the process completes. 
-//       If an error occurs, it will show up in this text box. - Contact Jaime for big errors
+//  Select the function you'd like to use from the dropdown menu above (next to Debug):
+//      - appendSingleRow: Appends the selected row to the last row of the new sheet 
+//        based on section and request count. (
+//        Notes:
+//           * Change variable "SpecificRow" to correct row number -> You'll find this in the "Variables" section below)
+//           * Make sure to click Save (Button to the left of "Run") before clicking Run
+//      - appendNewRows: Takes all rows in the sheet where the "duplicated" column (usually last column) isn't filled with an
+//        'X' and appends them to the new sheet based on section and request count.
+//      - appendAllRows: Takes all rows in the sheet and appends them to the new sheet 
+//        based on section and request count.
+//  Then Click run
+//      - A text box will pop up, this will inform you when the process completes. 
+//        If an error occurs, it will show up in this text box. - Contact Jaime for big errors
 
-var SourceName = "Applications";
-function appendRow() {
 
-  row = 4; //Change based on newest row -----------------------------------------------------
 
-  var sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SourceName); //Change to Source Sheet~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  var destinationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Section Duplicates");
+// Conversion Utility function -------------------------------------------------------------------------------------------------------------------------------------
 
-  sourceSheet.getRange(row, 65).check(); //65 is the row to check for duplication
-
-  var requests = 4; //4 Course Request fields, later function decrements to actualy count of row
-  var column = 25;
-  var cell = sourceSheet.getRange(row, column);
-  while (cell.isBlank()) { //Iterate until non-null request found
-    requests--;
-    cell = sourceSheet.getRange(row, (column -= 6));
-  };
-
-  var sectionColumn = 8; //Section Count Column
-
-  for (var j = 1; j <= requests; j++) {
-    var sectionCount = sourceSheet.getRange(row, sectionColumn).getValue();
-
-    // Get the number of columns in the source sheet
-    var lastColumn = sourceSheet.getLastColumn();
-
-    switch (j) {
-      case 1:
-        var includedColumnsRanges = [
-          { start: 1, end: 13 },
-          { start: 35, end: lastColumn }
-        ];
-        break;
-      case 2:
-        var includedColumnsRanges = [
-          { start: 1, end: 6 },
-          { start: 14, end: 20 },
-          { start: 35, end: lastColumn }
-        ];
-        break;
-      case 3:
-        var includedColumnsRanges = [
-          { start: 1, end: 6 },
-          { start: 21, end: 27 },
-          { start: 35, end: lastColumn }
-        ];
-        break;
-      case 4:
-        var includedColumnsRanges = [
-          { start: 1, end: 6 },
-          { start: 28, end: lastColumn }
-        ];
-        break
-    }
-
-    // Create an array to store the values of the desired rows
-    var values = [];
-
-    // Loop through each column and check if it falls within the included ranges
-    for (var col = 1; col <= lastColumn; col++) {
-      var isIncluded = includedColumnsRanges.some(function (range) {
-        return col >= range.start && col <= range.end;
-      });
-
-      if (isIncluded) {
-        var cellValue = sourceSheet.getRange(row, col).getValue();
-        values.push(cellValue);
-      }
-    }
-
-    // Append the values to the next empty rows in the destination sheet
-    for (var k = 0; k < sectionCount; k++) {
-      var lastRow = destinationSheet.getLastRow();
-      destinationSheet.getRange(lastRow + 1, 1, 1, values.length).setValues([values]);
-    }
-
-    sectionColumn += 7;
-  }
+function colNum(column) {
+  let depth = column.length - 1;
+  return (depth * 26) + letter.charCodeAt(depth) - 64;
 }
 
-function appendNewRows() {
 
-  var sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SourceName);
-  var destinationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Section Duplicates");
 
-  var columnNumber = 1;
+// Variables -------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  var dataRange = sourceSheet.getRange(1, columnNumber, sourceSheet.getLastRow(), 1);
-  var values = dataRange.getValues();
+const SpecificRow = 1; // For appendSingleRow() -> specify row to append
 
-  var count = 0;
-  for (var i = 0; i < values.length; i++) {
+const sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Applications"); //Source Sheet
+const destinationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Section Duplicates"); //Target Sheet
+
+const requestFieldCount = 7; // Number of fields in each specific course request (should be uniform)
+const lastColumn = sourceSheet.getLastColumn();
+
+const sectionColumnOffset = 1; //The section is usually the column next to the course request, but just in case <-
+const maxRequests = 4; //Will likely not change, but just in case
+
+const reqColumns = [colNum('G'), colNum('N'), colNum('U'), colNum('AB')]; //List of column numbers with Course Request Names
+const dCheckColumn = colNum('BM'); // Column with X's to check if a row has already been duplicated
+
+
+
+// Utility Functions -----------------------------------------------------------------------------------------------------------------------------------------------function filledCount(){
+
+function filledCount() {
+  let values = sourceSheet.getRange(1, 1, sourceSheet.getLastRow(), 1).getValues();
+  let count = 0;
+  for (let i = 0; i < values.length; i++) {
     if (values[i][0] !== "") {
       count++;
     }
   }
+  return count;
+}
 
+function setRequests(row) {
+  let requests = 4; // Max number of requests
+  let cell = sourceSheet.getRange(row, reqColumns[requests]);
+  while (cell.isBlank()) { //Iterate until non-null request found
+    requests--; // Decrement max number of requests
+    cell = sourceSheet.getRange(SpecificRow, reqColumns[requests]);
+  };
+  return requests;
+}
 
-  for (var row = 2; row <= count; row++) {
+function setSections(row, reqCol) {
+  let secCountCol = reqCol + sectionColumnOffset;
+  return sourceSheet.getRange(row, secCountCol).getValue();
+}
 
-    if (sourceSheet.getRange(row, 65).isBlank()) {
+function getRanges(i) {
+  let a = reqColumns[0] - 1;
+  let b = reqColumns[i - 1];
+  let c = reqColumns[i] - 1;
+  let d = reqColumns[reqCount] + requestFieldCount;
+  return includedColumnsRanges = [
+    { start: 1, end: a },
+    { start: b, end: c },
+    { start: d, end: lastColumn }
+  ];
+}
 
-      var requests = 4;
-      var column = 25;
-      var cell = sourceSheet.getRange(row, column);
-      while (cell.isBlank()) {
-        requests--;
-        cell = sourceSheet.getRange(row, (column -= 6));
-      };
-
-      var sectionColumn = 8;
-
-      for (var j = 1; j <= requests; j++) {
-
-        var sectionCount = sourceSheet.getRange(row, sectionColumn).getValue();
-
-        // Get the number of columns in the source sheet
-        var lastColumn = sourceSheet.getLastColumn();
-
-        switch (j) {
-          case 1:
-            var includedColumnsRanges = [
-              { start: 1, end: 13 },
-              { start: 35, end: lastColumn }
-            ];
-            break;
-          case 2:
-            var includedColumnsRanges = [
-              { start: 1, end: 6 },
-              { start: 14, end: 20 },
-              { start: 35, end: lastColumn }
-            ];
-            break;
-          case 3:
-            var includedColumnsRanges = [
-              { start: 1, end: 6 },
-              { start: 21, end: 27 },
-              { start: 35, end: lastColumn }
-            ];
-            break;
-          case 4:
-            var includedColumnsRanges = [
-              { start: 1, end: 6 },
-              { start: 28, end: lastColumn }
-            ];
-            break
-        }
-
-        // Create an array to store the values of the desired rows
-        var values = [];
-
-        // Loop through each column and check if it falls within the included ranges
-        for (var col = 1; col <= lastColumn; col++) {
-          var isIncluded = includedColumnsRanges.some(function (range) {
-            return col >= range.start && col <= range.end;
-          });
-
-          if (isIncluded) {
-            var cellValue = sourceSheet.getRange(row, col).getValue();
-            values.push(cellValue);
-          }
-        }
-
-        // Append the values to the next empty rows in the destination sheet
-        for (var k = 0; k < sectionCount; k++) {
-          var lastRow = destinationSheet.getLastRow();
-          destinationSheet.getRange(lastRow + 1, 1, 1, values.length).setValues([values]);
-        }
-
-        sectionColumn += 7;
-      }
+function buildRow(range) {
+  let values = []
+  // Loop through each column and check if it falls within the included ranges
+  for (let col = 1; col <= lastColumn; col++) {
+    if (range.includes(col)) {
+      let cellValue = sourceSheet.getRange(SpecificRow, col).getValue();
+      values.push(cellValue);
     }
-    sourceSheet.getRange(row, 65).setValue('X');
+  }
+  return values;
+}
+
+function appendTable(builtRow, sectionCount) {
+  // Append the values to the next empty rows in the destination sheet
+  for (let i = 0; i < sectionCount; i++) {
+    let lastRow = destinationSheet.getLastRow();
+    destinationSheet.getRange(lastRow + 1, 1, 1, values.length).setValues([builtRow]);
+  }
+}
+
+function appendRow(row) { // Main driver function
+  const reqCount = setRequests(row); // Get the number of requests
+
+  for (let i = 1; i <= reqCount; i++) {
+    let sectionCount = setSections(row, reqColumns[i - 1]);
+    let range = getRanges(i);
+    var builtRow = buildRow(range);
+    appendTable(builtRow, sectionCount);
+  }
+}
+
+
+
+// Main Functions --------------------------------------------------------------------------------------------------------------------------------------------------
+function appendSingleRow() {
+  sourceSheet.getRange(SpecificRow, dCheckColumn).check(); // Checks if this row has already been duplicated
+  appendRow(SpecificRow);
+}
+
+function appendNewRows() {
+
+  const count = filledCount();
+  for (var row = 2; row <= count; row++) {// Repeat process for each row in the original sheet
+    if (sourceSheet.getRange(row, dCheckColumn).isBlank()) {// Only duplicate NEW rows (Rows that do not have the duplicate checkbox marked)
+      appendRow(row);
+    }
+    sourceSheet.getRange(row, dCheckColumn).setValue('X');
   }
 }
 
 function appendAllRows() {
-
-  var sourceSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SourceName);
-  var destinationSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Section Duplicates");
-
-  var columnNumber = 1;
-
-  var dataRange = sourceSheet.getRange(1, columnNumber, sourceSheet.getLastRow(), 1);
-  var values = dataRange.getValues();
-
-  var count = 0;
-  for (var i = 0; i < values.length; i++) {
-    if (values[i][0] !== "") { // Check if rows are empty
-      count++;
-    }
-  }
-
+  const count = filledCount();
   for (var row = 2; row <= count; row++) {
-
-    var requests = 4;
-    var column = 25;
-    var cell = sourceSheet.getRange(row, column);
-    while (cell.isBlank()) {
-      requests--;
-      cell = sourceSheet.getRange(row, (column -= 6));
-    };
-
-    var sectionColumn = 8;
-
-    for (var j = 1; j <= requests; j++) {
-
-      var sectionCount = sourceSheet.getRange(row, sectionColumn).getValue();
-
-      // Get the number of columns in the source sheet
-      var lastColumn = sourceSheet.getLastColumn();
-
-      switch (j) {
-        case 1:
-          var includedColumnsRanges = [
-            { start: 1, end: 13 },
-            { start: 35, end: lastColumn }
-          ];
-          break;
-        case 2:
-          var includedColumnsRanges = [
-            { start: 1, end: 6 },
-            { start: 14, end: 20 },
-            { start: 35, end: lastColumn }
-          ];
-          break;
-        case 3:
-          var includedColumnsRanges = [
-            { start: 1, end: 6 },
-            { start: 21, end: 27 },
-            { start: 35, end: lastColumn }
-          ];
-          break;
-        case 4:
-          var includedColumnsRanges = [
-            { start: 1, end: 6 },
-            { start: 28, end: lastColumn }
-          ];
-          break
-      }
-
-      // Create an array to store the values of the desired rows
-      var values = [];
-
-      // Loop through each column and check if it falls within the included ranges
-      for (var col = 1; col <= lastColumn; col++) {
-        var isIncluded = includedColumnsRanges.some(function (range) {
-          return col >= range.start && col <= range.end;
-        });
-
-        if (isIncluded) {
-          var cellValue = sourceSheet.getRange(row, col).getValue();
-          values.push(cellValue);
-        }
-      }
-
-      // Append the values to the next empty rows in the destination sheet
-      for (var k = 0; k < sectionCount; k++) {
-        var lastRow = destinationSheet.getLastRow();
-        destinationSheet.getRange(lastRow + 1, 1, 1, values.length).setValues([values]);
-      }
-
-      sectionColumn += 7;
-    }
-    sourceSheet.getRange(row, 65).setValue('X');
+    appendRow(row);
   }
+  sourceSheet.getRange(row, dCheckColumn).setValue('X');
 }
-
-
